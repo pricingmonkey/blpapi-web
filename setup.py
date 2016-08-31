0625230745608b080059d210f9e5e783879b0831
Pawel Badenski <Pawel.Badenski@gmail.com>
2016-08-31 08:16:20 -0700

    Separate setup.py for dev and prod

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from distutils.core import setup
 import py2exe
-import os
 
 class Target:
     def __init__(self, **kw):
@@ -10,14 +9,6 @@ class Target:
         self.company_name = "Pricing Monkey"
         self.copyright = "Pricing Monkey"
         self.name = "Bloomberg Bridge"
-
-old_determine_dll_type = py2exe.dllfinder.DllFinder.determine_dll_type
-pack = ("msvcp140.dll", "vcruntime140.dll")
-def determine_dll_type(self, imagename):
-    if os.path.basename(imagename).lower() in pack:
-        return "EXT"
-    return old_determine_dll_type(self, imagename)
-py2exe.dllfinder.DllFinder.determine_dll_type = determine_dll_type
 
 standalone_server = Target(
     script = 'server.py',
