857ae9450b5bd4987d112bb7aceab57c94d89502
Pawel Badenski <pawel.badenski@gmail.com>
2016-09-26 09:24:25 +0100

    Fix formatting

--- server.py
+++ server.py
@@ -7,14 +7,14 @@ import json
 import traceback
 
 def main_is_frozen():
-   return (hasattr(sys, "frozen") or # new py2exe
-           hasattr(sys, "importers") # old py2exe
-           or imp.is_frozen("__main__")) # tools/freeze
+    return (hasattr(sys, "frozen") or # new py2exe
+        hasattr(sys, "importers") # old py2exe
+        or imp.is_frozen("__main__")) # tools/freeze
 
 def get_main_dir():
-   if main_is_frozen():
-       return os.path.dirname(sys.executable)
-   return os.path.dirname(os.path.realpath(__file__))
+    if main_is_frozen():
+        return os.path.dirname(sys.executable)
+    return os.path.dirname(os.path.realpath(__file__))
 
 BLOOMBERG_HOST = "localhost"
 BLOOMBERG_PORT = 8194
