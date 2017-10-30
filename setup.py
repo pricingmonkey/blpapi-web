378fc7b50fe6b385d8feee23c8e902bbfe24b114
Pawel Badenski <pawel.badenski@gmail.com>
2017-10-30 16:32:48 +0000

    Bump version to 2.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ class Target:
     def __init__(self, **kw):
         self.__dict__.update(kw)
 
-        self.version = "2.4.2"
+        self.version = "2.5.0"
         self.company_name = "Pricing Monkey Ltd"
         self.copyright = "Pricing Monkey Ltd"
         self.name = "Web API for Bloomberg Market Data"
