1a1dde74c652617f25fff7c3cdfcbda8d44d3734
Pawel Badenski <pawel.badenski@gmail.com>
2020-06-19 15:10:01 +0100

    fix: remove inter-test interaction

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,11 @@ import pytest
 from server import app as my_app, wireUpBlpapiImplementation
 from bridge.routes import dev
 
-@pytest.yield_fixture
+@pytest.yield_fixture(scope="function")
 def app():
     wireUpBlpapiImplementation(eventlet.import_patched("blpapi_simulator"))
     my_app.register_blueprint(dev.blueprint, url_prefix='/dev')
+    my_app.allSubscriptions = {}
     client = my_app.test_client()
     client.testing = True
     ctx = my_app.app_context()
